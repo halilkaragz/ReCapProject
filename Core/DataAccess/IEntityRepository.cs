1cb1dbdfe9e8f0e37c80f956c63c29a387c6cb65
halilkaragz <halilkaragoz_blsm@hotmail.com>
2021-10-26 17:21:42 +0300

    26.10.2021 - 17.20

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,12 +1,9 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     //Generic constaint
     //Class : referans tip olması için yazıldı
